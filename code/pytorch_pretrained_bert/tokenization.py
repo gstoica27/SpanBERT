a955c3a0bb83b53c8550e856b8535127a5ed4099
gstoica27 <gstoica27@gmail.com>
2020-09-19 16:17:06 -0700

    debugging vocab

--- code/pytorch_pretrained_bert/tokenization.py
+++ code/pytorch_pretrained_bert/tokenization.py
@@ -194,6 +194,7 @@ class BertTokenizer(object):
             max_len = PRETRAINED_VOCAB_POSITIONAL_EMBEDDINGS_SIZE_MAP[pretrained_model_name_or_path]
             kwargs['max_len'] = min(kwargs.get('max_len', int(1e12)), max_len)
         # Instantiate tokenizer.
+        print(resolved_vocab_file)
         tokenizer = cls(resolved_vocab_file, *inputs, **kwargs)
         return tokenizer
 
